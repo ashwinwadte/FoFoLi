5db23b744d1f2cbfb1fa416c64888666f258a984
Ashwin Wadte <ashwinwadte@gmail.com>
2016-02-21 17:02:15 +0530

    added EditText for password

--- app/src/main/java/com/waftinc/fofoli/CreateAccountActivity.java
+++ app/src/main/java/com/waftinc/fofoli/CreateAccountActivity.java
@@ -3,10 +3,12 @@ package com.waftinc.fofoli;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class CreateAccountActivity extends Activity {
 
     Button bCreateAccount;
+    EditText etName, etPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
