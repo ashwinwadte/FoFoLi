b2b095975a3c1172566cd4da5719354013494df4
Ashwin Wadte <ashwinwadte@gmail.com>
2016-02-21 15:45:46 +0530

    added Button

---
+++ app/src/main/java/com/waftinc/fofoli/CreateAccountActivity.java
@@ -0,0 +1,17 @@
+package com.waftinc.fofoli;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class CreateAccountActivity extends Activity {
+
+    Button bCreateAccount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+
+}
